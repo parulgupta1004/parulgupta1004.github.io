f45212fcbc917219e12a412527c9962015e26736
Parul Gupta <parul100495@gmail.com>
2020-05-26 19:44:13 -0400

    Corrected speaker link

--- src/containers/talks/talks.js
+++ src/containers/talks/talks.js
@@ -15,16 +15,13 @@ export default function Talks() {
           <div className="project-header">
             <h1 className="heading project-heading">Talks</h1>
             <p className="subTitle project-subtitle">SHARE AND SPREAD AWARENESS ON MACHINE LEARNING AND OTHER COOL TECH...</p>
-            <div className="project-cards-div">
               <div className="certificate-card1">
                 <div className="certificate-image-div">
                 <a href="https://www.womentech.net/speaker/Parul/Gupta" target="_blank"><img alt="Fairlearn" className="card-image2" src={require("../../assests/images/speaker2.png")}></img></a>
                 </div>
-                <div className="certificate-detail-div">
-                <h4 onClick={() => openUrlInNewTab("https://www.womentech.net/speaker/Parul/Gupta")} className="card-title">WomenTech Global Conference 2020</h4>
-                <p onClick={() => openUrlInNewTab("https://www.womentech.net/speaker/Parul/Gupta")} className="card-subtitle3"><b>Topic</b>: The (Un)Fair Machine Learning</p>
+                <div className="certificate-card-footer">
+                  <p onClick={() => openUrlInNewTab("https://www.womentech.net/speaker/Parul/Gupta")}><b>WomenTech Global Conference 2020</b></p>
                 </div>
-              </div>
             </div>
           </div>
         </div>
