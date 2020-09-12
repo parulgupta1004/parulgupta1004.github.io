3dbcbff25abb2d6ede63e6bfbe128fcdd69230fe
Parul Gupta <parul100495@gmail.com>
2020-09-12 14:03:12 -0400

    Revert "Updates"
    This reverts commit 30379e7f144897f3653145d871e61c6280855e51.

---
+++ src/containers/Main.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import Header from "../components/header/Header";
+import Greeting from "./greeting/Greeting";
+import Academics from "./academics/Academics";
+import StackProgress from "./skillProgress/skillProgress";
+import Projects from "./projects/Projects";
+import WorkEx from "./Workex/Workex";
+import Blogs from "./blogs/Blogs";
+import Footer from "../components/footer/Footer";
+import Top from "./topbutton/Top";
+import Talks from "./talks/talks";
+import Scholarship from "./scholarship/scholarship"
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Greeting />
+        <Academics />
+        <Projects />
+        <WorkEx />
+        <Talks />
+        <Scholarship />
+        <StackProgress />
+        <Blogs />
+        <Footer />
+        <Top />
+      </div>
+    );
+  }
+}
