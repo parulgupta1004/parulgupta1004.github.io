33f4c3c9feb9eee146a63ba611ba6a177edbd3ce
Parul Gupta <parul100495@gmail.com>
2025-01-20 10:07:13 -0800

    Updates

---
+++ precache-manifest.8164d11a943311770e563a8c0a76dd2e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1354ab43028107b1b85e6954ffdfe6bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64fd254d83bd03ac47c1",
+    "url": "/static/css/main.5842aa8d.chunk.css"
+  },
+  {
+    "revision": "c3f6dc0a041f13aac4f9",
+    "url": "/static/js/2.33e25336.chunk.js"
+  },
+  {
+    "revision": "64fd254d83bd03ac47c1",
+    "url": "/static/js/main.cec3569c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "46642aecfd833d90a87c78dfda72e315",
+    "url": "/static/media/arcesium.46642aec.png"
+  },
+  {
+    "revision": "1c26e4ee2b83cc32a4dc3598a1eeb841",
+    "url": "/static/media/books.1c26e4ee.svg"
+  },
+  {
+    "revision": "06e3487242f9031c21b8971c282b26d7",
+    "url": "/static/media/brainpan.06e34872.png"
+  },
+  {
+    "revision": "ef74b94769da468cae0178ed683b7f11",
+    "url": "/static/media/covid.ef74b947.png"
+  },
+  {
+    "revision": "23ff169fe6cff0c029b175f50f3ef49b",
+    "url": "/static/media/fairlearn.23ff169f.png"
+  },
+  {
+    "revision": "1b64eb169a8e20a5e5765e3bfac9c0fb",
+    "url": "/static/media/genes.1b64eb16.png"
+  },
+  {
+    "revision": "3d1807703d15bfc110035aec17fe5f5e",
+    "url": "/static/media/neural.3d180770.png"
+  },
+  {
+    "revision": "0a602df062af6dc8870c00fb9eaa39cd",
+    "url": "/static/media/optum.0a602df0.png"
+  },
+  {
+    "revision": "2d8ba07768d4434c62a8b87b19da6efa",
+    "url": "/static/media/parul_gupta_website.2d8ba077.jpg"
+  },
+  {
+    "revision": "9be88ea7e9e53f2a63e6541f7c44bb89",
+    "url": "/static/media/senti.9be88ea7.png"
+  },
+  {
+    "revision": "4ca8e049a60bb08fab196a38b8f5b58a",
+    "url": "/static/media/woman.4ca8e049.png"
+  },
+  {
+    "revision": "c3b082aa1d846d909196c346c4edf69e",
+    "url": "/static/media/worldquant.c3b082aa.png"
+  }
+]);
