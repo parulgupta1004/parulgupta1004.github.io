0e8f993dd624e9b494066fb00194f1994e2ec0e1
Parul Gupta <parul100495@gmail.com>
2025-01-27 14:42:50 -0500

    Revert "Add tracker"
    This reverts commit 6fa66d88b8bfc70e2de70a1528fead60a6055a74.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,6 @@ import "./App.css";
 import Main from "./containers/Main";
 
 function App() {
-    // setup google analytics
-    var _gaq = _gaq || [];
-    _gaq.push(['parulgupta_website._setAccount', 'G-ZKFQ0EFNKY']);
-    _gaq.push(['parulgupta_website._trackPageview']);
-
-    (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = 'https://www.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
   return (
     <div>
       <Main />
