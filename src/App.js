bba424d55e6dfa8255f35afcd863364bd1298811
Parul Gupta <parul100495@gmail.com>
2025-12-15 22:03:43 -0800

    Add json LD to the website

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Main from "./containers/Main";
 import ReactGA from 'react-ga';
-
+import JsonLd from './jsonld.js';
 
 function App() {
     // setup google analytics
@@ -10,9 +10,12 @@ function App() {
     ReactGA.pageview(window.location.pathname + window.location.search);
 
   return (
+    <>
+    <JsonLd />
     <div>
       <Main />
     </div>
+    </>
   );
 }
 
