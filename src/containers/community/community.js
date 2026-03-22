b3bfcbfcf77691d562a5825de9452e369d223bac
Parul Gupta <parul100495@gmail.com>
2026-03-22 14:24:58 -0700

    Revert "Updates"
    This reverts commit d536a17a5fb238ed1fe7470081a75d7066ab7322.

---
+++ src/containers/community/community.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "../projects/Projects.css";
+import "./community.css";
+import { Fade } from "react-reveal";
+export default function Community() {
+
+  return (
+    <Fade bottom duration={1000}>
+      <div className="main" id="community">
+        <div className="project-main-div">
+          <div className="project-header">
+            <h1 className="heading project-heading">Community Engagements</h1>
+            <div>
+            {/* <p><b>Sep 2025</b>: Reviewer @ <a target="_blank" rel="noopener noreferrer" href="https://tapiaconference.cmd-it.org/" className="no_decor">Tapia 2025</a></p> */}
+            <p><b>Jul 2025</b>: Reviewer @ <a target="_blank" rel="noopener noreferrer" href="https://ep2025.europython.eu/" className="no_decor">EuroPython 2025</a></p>
+            <p><b>Jul 2025</b>: Reviewer @ <a target="_blank" rel="noopener noreferrer" href="https://www.scipy2025.scipy.org/" className="no_decor">Scipy 2025</a></p>
+            <p><b>May 2025</b>: PyLadies Auction Volunteer @ <a target="_blank" rel="noopener noreferrer" href="https://us.pycon.org/2025/" className="no_decor">PyCon US 2025</a></p>
+            {/* <p><b>Apr 2025</b>: Judge @ <a target="_blank" rel="noopener noreferrer" href="https://ai-valley-hackathon.devpost.com/" className="no_decor">AI Valley Hackathon 2025</a>, Palo Alto, CA</p> */}
+            <p><b>Apr 2025</b>: Judge @ <a target="_blank" rel="noopener noreferrer" href="https://sfhacks.io/" className="no_decor">SF Hacks 2025</a> (Theme - Tech For Good), San Francisco, CA</p>
+            <p><b>Mar 2025</b>: Expo @ <a target="_blank" rel="noopener noreferrer" href="https://www.socallinuxexpo.org/scale/22x" className="no_decor">SCaLE 22x 2025</a></p>
+            <p><b>Feb - Mar 2025</b>: Mentor @ <a target="_blank" rel="noopener noreferrer" href="https://labs.codeday.org/" className="no_decor">CodeDay Labs</a></p>
+            <p><b>Feb 2025</b>: Judge @ <a target="_blank" rel="noopener noreferrer" href="https://gdg.community.dev/events/details/google-gdg-on-campus-eastern-washington-university-cheney-united-states-presents-build-with-ai-codequest-2025/" className="no_decor">Build With AI: CodeQuest 2025</a>, GDG, Eastern Washington University</p>
+            <p><b>Feb 2025</b>: Judge @ <a target="_blank" rel="noopener noreferrer" href="https://hackforhumanity.io/" className="no_decor">Hack For Humanity 2025</a>, Santa Clara, CA</p>
+            {/* <p><b>Feb 2025</b>: Judge @ <a target="_blank" rel="noopener noreferrer" href="https://gdg.community.dev/events/details/google-gdg-on-campus-higher-school-of-communication-of-tunis-supcom-ariana-tunisia-presents-ai-odyssey-hackathon/cohost-gdg-on-campus-higher-school-of-communication-of-tunis-supcom-ariana-tunisia" className="no_decor">AI Odyssey Hackathon 2025</a>, GDG, Sup'Com, Ariana, Tunisia</p> */}
+            <p><b>Jan 2025</b>: Career Exploration Lounge @ <a target="_blank" rel="noopener noreferrer" href="https://rewritingthecode.org/event/tech-connect-tech-exploration-recruiting-event/" className="no_decor">Re-writing The Code - Tech Connect 2025</a></p>
+            <p><b>Jun-Aug 2024</b>: Mentor @ <a target="_blank" rel="noopener noreferrer" href="https://cicscareers.notion.site/Alumni-Mentorship-Program-AMP-87a61d3fe26c4c1ba688d137609881f7" className="no_decor">Alumni Mentorship Program (AMP) 2024</a>, UMass CICS</p>
+            {/* <p><b>Jan 2025</b>: Featured @ <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/posts/womentech-network_womenintech-networking-motivation-activity-7290453345729761281-EIcw" className="no_decor">WomenTech Network Highlights 2025</a></p> */}
+            {/* <p><b>Dec 2024</b>: <a target="_blank" rel="noopener noreferrer" href="https://conference.pyladies.com/" className="no_decor">PyLadies Con 2024</a></p>
+            <p><b>Oct 2024</b>: <a target="_blank" rel="noopener noreferrer" href="https://discuss.python.org/t/parul-gupta-and-jason-fried-added-to-the-triage-team/67686" className="no_decor">Python Triager Open-Source</a> @ Python Software Foundation</p>
+            <p><b>Sep 2024</b>: <a target="_blank" rel="noopener noreferrer" href="https://discuss.python.org/t/2024-core-dev-sprint-in-bellevue-wa-september-23-27/39227" className="no_decor">Python Core Dev Sprints 2024</a></p>
+            <p><b>Jul 2024</b>: <a target="_blank" rel="noopener noreferrer" href="https://www.meetup.com/pyninsula-python-peninsula-meetup/events/301678708/" className="no_decor">Python Pyninsula Meetup 2024</a></p>
+            <p><b>Jun 2024</b>: <a target="_blank" rel="noopener noreferrer" href="https://2024.northbaypython.org/" className="no_decor">North Bay Python 2024</a></p> */}
+            <p><b>May 2024</b>: Session Runner @ <a target="_blank" rel="noopener noreferrer" href="https://us.pycon.org/2024/" className="no_decor">PyCon US 2024</a></p>
+            <p><b>Mar 2024</b>: Expo @ <a target="_blank" rel="noopener noreferrer" href="https://www.socallinuxexpo.org/scale/21x" className="no_decor">SCaLE 21x 2024</a></p>
+            <p><b>Sep 2023</b>: Reviewer @ <a target="_blank" rel="noopener noreferrer" href="https://ghc.anitab.org/" className="no_decor">Grace Hopper Celebration 2023</a></p>
+            <p><b>Jun 2022</b>: <a target="_blank" rel="noopener noreferrer" href="https://rstats.ai/" className="no_decor">2022 New York R Conference</a></p>
+            <p><b>Jun 2021 - 2025</b>: Mentor @ <a target="_blank" rel="noopener noreferrer" href="https://rewritingthecode.org/" className="no_decor">Rewriting The Code</a></p>
+            <p><b>Feb 2021</b>: Communications Lead @ <a target="_blank" rel="noopener noreferrer" href="https://groups.cs.umass.edu/voicesofds21/" className="no_decor">Voices of Data Science</a>, University of Massachusetts Amherst</p>
+            <p><b>Oct 2019</b>: <a target="_blank" rel="noopener noreferrer" href="https://yahoofinanceams2019.splashthat.com/" className="no_decor">Yahoo Finance All Markets Summit 2019: Generational Opportunities</a></p>
+            <p><b>Oct 2018</b>: <a target="_blank" rel="noopener noreferrer" href="https://in.pycon.org/2018/" className="no_decor">PyCon India 2018</a></p>
+            {/* <p><b>Mar 2016</b>: Represented IIT Indore @ <a target="_blank" rel="noopener noreferrer" href="https://nif.org.in/foin-2016" className="no_decor">Festival of Innovation 2016, Department of Science and Technology, Government of India at Rastrapati Bhawan (President's House), India</a></p>
+            <p><b>Dec 2015</b>: Represented IIT Indore @ <a target="_blank" rel="noopener noreferrer" href="https://pib.gov.in/newsite/PrintRelease.aspx?relid=131767" className="no_decor">India International Science Fair 2015, Ministry of Science and Technology, Government of India held at IIT Delhi</a></p> */}
+            </div>
+            </div>
+          </div>
+      </div>
+    </Fade>
+  );
+}
